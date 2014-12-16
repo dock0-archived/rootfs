135227db2f4dec18d64c0ba76ab3fae6e36ab62e
Les Aker <me@lesaker.org>
2014-12-16 00:08:47 -0500

    add s6-linux-utils

--- scripts/s6.rb
+++ scripts/s6.rb
@@ -4,7 +4,7 @@
 require 'open-uri'
 require 'tempfile'
 
-['execline', 's6'].each do |package|
+['execline', 's6', 's6-linux-utils'].each do |package|
   version = @config['versions'][package]
   url = "https://github.com/akerl/#{package}/releases/download/#{version}/#{package}.tar.gz"
   file = Tempfile.new(package)
