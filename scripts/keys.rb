423bde7e35446207a2737afc5e4128ae0763017b
Les Aker <me@lesaker.org>
2015-04-17 14:25:54 -0400

    update key handling to be smart

--- scripts/keys.rb
+++ scripts/keys.rb
@@ -2,15 +2,28 @@
 # Add SSH keys
 
 require 'fileutils'
-require 'open-uri'
+require 'tmpdir'
 
-keys = @config.fetch(:keys, []).map do |url|
-  open(url, 'rb') { |req| req.read }
+
+def list_keys(path)
+  Dir.glob("#{path}/**/*").select { |x| File.file? x }
 end
+
+def load_keys(path)
+  list_keys(path).map { |x| File.readlines x }.flatten.uniq
+end
+
+def fetch_keys
+  Dir.mktmpdir('keyrepo') do |dir|
+    `git clone #{@options[:keys][:repo]} #{dir}`
+    group_path = File.join(dir, @options[:keys].fetch(:group, 'default'))
+    load_keys(group_path)
+  end
+end
+
 path = "#{@paths[:build]}/root/.ssh/authorized_keys"
 
 FileUtils.mkdir_p File.dirname(path)
 File.open(path, 'wb') do |fh|
-  keys.each { |key| fh << key + "\n" }
+  fetch_keys.each { |key| fh << key + "\n" }
 end
-
